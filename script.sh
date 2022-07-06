6bf930c6949f40295f281ac271565397bada18b2
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-07-06 02:49:08 +0200

    add comments

--- script.sh
+++ script.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# Extract /etc files
 tar -xvf ./etc.tar.xz
+
+# Necessary for podman
 touch /etc/subuid /etc/subgid
-usermod --add-subuids 100000-165535 --add-subgids 100000-165535 jan
+usermod --add-subuids 100000-165535 --add-subgids 100000-165535 jan 
+
+# Copy over custom /etc files
 cp ./etc/* /etc -r
+
+# Generate locale
 locale-gen
+
+# System services
 systemctl enable zram
 systemctl enable bluetooth
