260ad9ceb2d3df836de36b726086d5ec6358c0e4
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-08-17 17:36:14 +0200

    updated for xfce

--- generate-config.sh
+++ generate-config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-comm -23 <(pacman -Qqett | sort) <(pacman -Qqg base-devel -g gnome -g gnome-extra | sort | uniq) > pacman-list
+# For gnome - comm -23 <(pacman -Qqett | sort) <(pacman -Qqg base-devel -g gnome -g gnome-extra | sort | uniq) > pacman-list
+comm -23 <(pacman -Qqett | sort) <(pacman -Qqg base-devel -g xfce4 -g xfce4-goodies | sort | uniq) > pacman-list
 flatpak --app list --columns=app > flatpak-list
